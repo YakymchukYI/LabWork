d70f87fbf70fb6c5a34c0ff22a1f41588acb8dce
YakymchukYI <yaroslav.yakymchuk27@gmail.com>
2018-04-17 15:30:11 +0300

    LR14 start

---
+++ LR14/LR14/LR14.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<ctime>
+#include<string>
+using namespace std;
+
+void sayhallo(string);
+void sayhallo(string, int);
+void holyday(int, int);
+
+
+int main()
+{
+	/*sayhallo("Ivan");
+	sayhallo("Vasyl");
+	sayhallo("Bye");*/
+
+	/*string name;
+	int num;
+	cout << "Enter name: "; getline(cin, name);
+	cout << "Enter number of repit: "; cin >> num;
+	sayhallo(name, num);*/
+
+	/*char symbol;
+	int day, month;
+	cout << "Enter day and month (Example 07.01) of yors hollyday > ";
+	cin >> day >> symbol >> month;
+	holyday(day, month);*/
+
+
+	system("pause");
+	return 0;
+}
+
+void sayhallo(string name)
+{
+	cout << "Hello, " << name << "!\n";
+}
+
+void sayhallo(string name, int num)
+{
+	for (int i = 0; i < num; i++) {
+		cout << "Hello, " << name << "!\n";
+	}
+}
+void holyday(int day, int month)
+{
+	if (day == 7 && month == 1) {
+		cout << "Rizdvo!\n";
+	}
+	else if (day == 27 && month == 2) {
+		cout << "Birtday!\n";
+	}
+	else if (day == 8 && month == 3) {
+		cout << "8 Bereznya!\n";
+	}
+}
